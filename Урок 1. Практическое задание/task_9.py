a47c63d3c6abb372f388fef3587f1b03155867c1
jadorex3 <dron9600@mail.ru>
2020-03-27 16:06:58 +0300

    Perform the task_9.py

--- Урок 1. Практическое задание/task_9.py
+++ Урок 1. Практическое задание/task_9.py
@@ -4,3 +4,24 @@
 
 Подсказка: можно добавить проверку, что введены равные числа
 """
+
+
+import sys
+
+try:
+    NUM_1 = int(input('Введите число a = '))
+    NUM_2 = int(input('Введите число b = '))
+    NUM_3 = int(input('Введите число c = '))
+
+    if NUM_1 == NUM_2 or NUM_1 == NUM_3 or NUM_2 == NUM_3:
+        print('Введены равные числа')
+        sys.exit(1)
+    elif NUM_2 < NUM_1 < NUM_3 or NUM_3 < NUM_1 < NUM_2:
+        print(f'Среднее число: {NUM_1}')
+    elif NUM_1 < NUM_2 < NUM_3 or NUM_3 < NUM_2 < NUM_1:
+        print(f'Среднее число: {NUM_2}')
+    else:
+        print(f'Среднее число: {NUM_3}')
+
+except ValueError:
+    print("Введенное значение не является целым числом")
